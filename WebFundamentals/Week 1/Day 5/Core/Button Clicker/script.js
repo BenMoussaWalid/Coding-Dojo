ae07c9051d464879eb78a63382ccec43dec75d80
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-02 21:51:17 +0100

    commit

---
+++ WebFundamentals/Week 1/Day 5/Core/Button Clicker/script.js
@@ -0,0 +1,13 @@
+var efface=document.querySelector(".but3");
+function AA() {efface.remove();
+    
+}
+var changee=document.querySelector("#btn");
+function change() {
+    changee.inneText="logout";
+}
+var alerttt=document.querySelector(".but2");
+function alertt() {
+    alert("ninja was like")
+    
+}
