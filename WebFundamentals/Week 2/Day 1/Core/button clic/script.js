9a6d7a2b9726f2fc1a1557903ced87a380586bc4
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-07 21:37:56 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Core/button clic/script.js
@@ -0,0 +1,10 @@
+var element=document.querySelector("#but1");
+function change(){
+    element.innerText="logout"   
+}
+function deletee(element){
+    element.remove();
+}
+function message(element){
+    alert(" Ninja was likes  ");
+}
