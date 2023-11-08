08c8bd64a02c9ce6cfaf9f18df518afe4fb125c8
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-08 11:44:37 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Practice/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+var element=document.querySelector("#aa");
+function over() {
+    element.src = "vd1.mp4";
+    element.play();
+}
+var element1=document.querySelector("#aa");
+function out() {
+    element1.src = "vd1.mp4";
+    element1.pause();
+}
