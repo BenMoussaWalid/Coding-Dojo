7afc6523ccc61022e7134d58f1066e319ccc352a
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-08 11:54:52 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 2/Core/linking likess/Liking Likes 2/like.js
@@ -0,0 +1,18 @@
+var count=0;
+var message=document.querySelector("#cc");
+function like(){
+    count=count+1;
+    message.innerText="I have "+ count + " likes";
+}
+var count2=0;
+var message2=document.querySelector("#zz");
+function like2(){
+    count2=count2+1;
+    message2.innerText="I have "+ count2 + " likes";
+}
+var count3=0;
+var message3=document.querySelector("#dd");
+function like3(){
+    count3=count3+1;
+    message3.innerText="I have "+ count3 + " likes";
+}
