d4a408794dff6bd3d3bd2e139600dad7b2f3527d
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-16 13:54:54 +0100

    commit

---
+++ Python/Week 1/Day 1/Practice/HelloWorld.py
@@ -0,0 +1,15 @@
+# 1. TASK: print "Hello World"
+print( 42 )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( 42 )	# with a comma
+print(  )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( 42 )	# with a comma
+print(  )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( 42) # with .format()
+print( 42) # with an f string
---
+++ Python/Week 1/Day 1/fundamentals.py
@@ -0,0 +1,36 @@
+# Fundamentals
+# this is a comment !
+"""this is a multiline
+    comment 
+    """
+  # * VARIABLES 
+# PRIMITIVES 
+
+import numbers
+
+
+My_float =3.14  
+my_name ="alice"
+is_admin =False 
+my_num =11
+# snake_case
+format_string = f"we can inject {my_num} variable here ."
+print(format_string)
+
+#? ==== composte types ===
+
+
+#=== LISTS
+#          01 2 3 4 5 6 
+num_list =[2,3,6,9,8,4]
+
+#print(num_list[4])
+
+#    apprend a number to the list 
+num-list.append(101)
+print(num_list)
+
+
+# remove en alement from the end of list
+removed = num_list.pop()
+print(removed)
