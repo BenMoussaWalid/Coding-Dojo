f334cffb332f631f974d41ddefcfbf691c805a42
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-22 20:39:22 +0100

    commit

---
+++ Python/Week 2/Day 2/Core/Dojos and Ninja/Untitled.sql
@@ -0,0 +1,19 @@
+select * from dojos ;
+insert into dojos (names)
+values ("walid"),("amen"),("chaima");
+
+
+delete from dojos 
+where id in (1,2,3 ) ;
+
+insert into dojos (names)
+values ("aziz") ,("amen"),("chaima");
+
+
+insert into ninjas (names)
+values ("aziz") ,("amen"),("chaima");
+
+
+
+
+
