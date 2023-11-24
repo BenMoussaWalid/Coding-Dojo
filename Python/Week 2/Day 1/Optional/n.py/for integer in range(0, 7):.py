60d2c989f77c20c0bd82e5f08085835e472ff7e8
BenMoussaWalid <benmoussawalid@icloud.com>
2023-11-24 14:37:27 +0100

    commit

---
+++ Python/Week 2/Day 1/Optional/n.py/for integer in range(0, 7):.py
@@ -0,0 +1,4 @@
+def multiply(num_list, num): #don't go inside the function until the function is called
+  a = [2,4,10,16]
+  b = multiply(a,5) # now our function executes; what is a function call equal to?
+  print(b)
---
+++ Python/Week 2/Day 2/Practice/books/Untitled.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `books SQL`.books;
+insert into  users ("first_name","last_name") value ("Jane"," Amsden")(" Emily"," Dixon")
+(" Theodore"," Dostoevsky")(" William"," Shapiro")(" Lao", "Xiu")
+
